08362d835e682b1673f806bf6c0946f9c8989e88
VanshMehta588 <161753906+VanshMehta588@users.noreply.github.com>
2024-12-17 16:30:37 +0530

    ..

--- src/components/UserInterface.js
+++ src/components/UserInterface.js
@@ -12,7 +12,7 @@ export const UserInterface = () => {
                     </div>
                 </div>
                 <div className="row mb-4">
-                    <div className="col-4">
+                    <div className="col-12 col-md-4">
                         <label htmlFor="name" className="mb-1 fw-bold">
                             Name
                         </label>
@@ -22,7 +22,7 @@ export const UserInterface = () => {
                             placeholder="Enter full name"
                         />
                     </div>
-                    <div className="col-4">
+                    <div className="col-12 col-md-4">
                         <label htmlFor="name" className="mb-1 fw-bold">
                             Phone number
                         </label>
@@ -32,7 +32,7 @@ export const UserInterface = () => {
                             placeholder="Add contact number"
                         />
                     </div>
-                    <div className="col-4">
+                    <div className="col-12 col-md-4">
                         <label htmlFor="name" className="mb-1 fw-bold">
                             Email ID
                         </label>
